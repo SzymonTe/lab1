2cd0b2a380c753d1161f7cad0983af641086e601
student <student@debian>
2016-04-14 19:12:50 +0200

    zadanie

--- main.cpp
+++ main.cpp
@@ -6,18 +6,31 @@ char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
-{
-	int n;
+{	
+	long int seed = atoi(*(argv+2));
+	srand(time((argc == 3) ? &seed : NULL));
+	int n = atoi(*(argv+1));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char*[n];
+	for(int i = 0; i < n; i++)
+	
+		square[i] = new char [n];
+
+	for(int i = 0; i < n*n; i++)
+			
+		square[i/n][i%n] = rand()%('a' - 'z' + 1) + 'a';
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
-{
+{	
+	for(int i = 0; i < n*n; i++)
+	
+		printf("%c %c",square[i/n][i%n], (i%n == n-1) ? '\n' : ' ');
 
 }
